6347639b7d253d60eaa9731658ed270d2771b1b9
tobias-ahlund <tobias.ahlund@gmail.com>
2023-10-18 12:26:55 +0200

    ProductsWrapper responsive

--- turn-tables/resources/js/Components/ProductsWrapper.style.jsx
+++ turn-tables/resources/js/Components/ProductsWrapper.style.jsx
@@ -4,6 +4,16 @@ export const ProductsWrapper = styled.section`
     width: calc(100% - 4rem);
     margin: 2rem auto;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(1, 1fr);
     gap: 2rem;
+    @media (min-width: 500px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media (min-width: 800px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+    @media (min-width: 1600px) {
+        grid-template-columns: repeat(4, 1fr);
+        max-width: 1600px;
+    }
 `
