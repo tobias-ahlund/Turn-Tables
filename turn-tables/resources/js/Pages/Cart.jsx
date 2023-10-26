7ecfe038217ca3d9e3eaf4734a170eb72cdef639
tobias-ahlund <tobias.ahlund@gmail.com>
2023-10-26 13:31:03 +0200

    cart products flexbox

--- turn-tables/resources/js/Pages/Cart.jsx
+++ turn-tables/resources/js/Pages/Cart.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { useShoppingCart } from 'use-shopping-cart';
 import { urlFor } from '@/turn-table-studio/utils/sanity.client';
 import DefaultLayout from '@/Layouts/DefaultLayout';
+import styled from 'styled-components';
+
+const ProductCartWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid #ccc;
+  padding: 1rem;
+`;
+
+const ProductImage = styled.img`
+  max-width: 100px; 
+  margin-right: 1rem;
+`;
 
 export default function Cart() {
   const { cartDetails, cartCount, totalPrice, removeItem, clearCart } = useShoppingCart();
@@ -18,14 +32,16 @@ export default function Cart() {
         <p>Total Price: {totalPrice} SEK</p>
 
         {Object.values(cartDetails).map((item) => (
-            <div key={item.id}>
+            <ProductCartWrapper key={item.id}>
+              <ProductImage src={urlFor(item.image)} alt="Image of the product." />
+              <div>
                 <p>{item.name}</p>
                 <p>Price: {item.price} SEK</p>
                 <p>Total price: {item.value} SEK</p>
                 <p>Quantity: {item.quantity}</p>
-                <img src={urlFor(item.image)} alt="Image of the product." />
                 <button onClick={() => handleRemoveItem(item)}>Remove from Cart</button>
-            </div>
+              </div>
+            </ProductCartWrapper>
         ))}
 
         <button onClick={clearCart}>Clear Cart</button>
