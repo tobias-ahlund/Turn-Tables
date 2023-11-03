1cd5b7b308f9cedb239e0e62510613acf3a87a4e
tobias-ahlund <tobias.ahlund@gmail.com>
2023-11-03 14:57:47 +0100

    Update Search.jsx

--- turn-tables/resources/js/Pages/Search.jsx
+++ turn-tables/resources/js/Pages/Search.jsx
@@ -3,6 +3,11 @@ import { fetchAllProducts } from "@/turn-table-studio/utils/sanity.queries";
 import { useState, useEffect } from 'react';
 import { ProductsWrapper } from '@/Components/ProductsWrapper.style';
 import ProductCard from '@/Components/ProductCard.style';
+import styled from 'styled-components';
+
+const ProdCount = styled.h2`
+    margin-bottom: 2rem;
+`;
 
 export default function Search({ wishlistItems }) {
     const [products, setProducts] = useState([]);
@@ -44,11 +49,11 @@ export default function Search({ wishlistItems }) {
             <DefaultLayout>
                 {productSearch && filteredProducts.length !== 0 && (
                     <>
-                        <h2>Showing results for "{productSearch}"</h2>
-                        <p>We found {filteredProducts.length} product{filteredProducts.length > 1 && "s"}.</p>
+                        <h1>Showing results for "{productSearch}"</h1>
+                        <ProdCount>We found {filteredProducts.length} product{filteredProducts.length > 1 && "s"}.</ProdCount>
                     </>
                 )}
-                {productSearch && filteredProducts.length === 0 && <h2>No results for "{productSearch}"</h2>}
+                {productSearch && filteredProducts.length === 0 && <h1>No results for "{productSearch}"</h1>}
                 <ProductsWrapper>
                     {productSearch && filteredProducts.map((product) => (
                         <ProductCard
