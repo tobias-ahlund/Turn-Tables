30510bc4c007db722a1d39ad186b12ef06c5b65e
tobias-ahlund <tobias.ahlund@gmail.com>
2023-11-17 11:06:00 +0100

    Update LogoText.jsx

--- turn-tables/resources/js/Components/LogoText.jsx
+++ turn-tables/resources/js/Components/LogoText.jsx
@@ -1,13 +1,30 @@
 import { Link } from '@inertiajs/react';
+import LogoImage from '@/public/images/logo.svg';
+import styled from 'styled-components';
+
+const LogoWrapper = styled.div`
+    & > a {
+        display: flex;
+        align-items: center;
+        font-weight: bold;
+        gap: .5rem;
+    }
+
+    & > a > img {
+        width: 2.5rem !important;
+        height: 2.5rem !important;
+    }
+`
 
 export default function Logo() {
     return (
-        <div>
+        <LogoWrapper>
             <Link
                 href={route('home')}
             >
+                <img src={LogoImage} alt="logo" />
                 <p>Turn Tables</p>
             </Link>
-        </div>
+        </LogoWrapper>
     );
 }
